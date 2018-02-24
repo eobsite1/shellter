cccc2125d2e0f91b0f52dc4d16ee98766b29007c
s0lrImplosion <36793165+s0lrImplosion@users.noreply.github.com>
2018-02-24 18:33:50 +0000

    Update Installer.sh
    Fixed verbose install, still need to fix normal install

--- Installer.sh
+++ Installer.sh
@@ -12,30 +12,19 @@ clear
 case $1 in
 	v)
 		echo "Installing..."
-		echo "cp -a ./. ~/Shellter"
-		cp -a ./. ~/Shellter
+		sleep 1
+		echo "git clone https://github.com/NightVisionGroup/shellter.git ~/Shellter"
+		git clone https://github.com/NightVisionGroup/shellter.git ~/Shellter
 		sleep 1
 		echo "chmod -R 755 ~/Shellter/"
-		chmod -R 755 ~/Shellter/
-		sleep 1
-		echo "touch ~/.bashrc"
-		touch ~/.bashrc
-		sleep 1
-		echo "alias shellter=~/Shellter/run.sh"
-		echo "alias shellter=~/Shellter/run.sh" >> ~/.bashrc
-		sleep 1
-		echo "rm ~/Shellter/Installer.sh"
-		rm ~/Shellter/Installer.sh
-		sleep 1
+		chmod -R 755 ~/Shelter/
+		echo "sh ~/Shellter/postinstall.sh"
+		sh ~/Shellter/postinstall.sh
+		break
 		;;
 	*)
 		echo "Installing..."
-		cp -a ./. ~/Shellter
-		chmod -R 755 ~/Shellter/
-		touch ~/.bashrc
-		echo "alias shellter=~/Shellter/run.sh" >> ~/.bashrc
-		rm ~/Shellter/Installer.sh
-		sleep 1
+
 		;;
 esac
 echo "Done!"
