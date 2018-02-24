e339476d93d03859132431ef2bbe7258f5b60c46
s0lrImplosion <36793165+s0lrImplosion@users.noreply.github.com>
2018-02-24 22:03:01 +0000

    Update postinstall.sh
    moving alias creation

--- postinstall.sh
+++ postinstall.sh
@@ -10,6 +10,4 @@ echo "git pull" >> Updater.sh
 echo "sh ./postinstall.sh" >> Updater.sh
 chmod 777 ~/Shellter/Updater.sh
 cp ./Updater.sh ~/Desktop/
-touch ~/.bashrc
-echo "alias shellter=~/Shellter/run.sh" >> ~/.bashrc
-echo "alias shellterupdate=~/Shellter/Updater.sh" >> ~/.bashrc
+
