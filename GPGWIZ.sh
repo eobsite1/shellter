ea737b96da553acd40df0471d4f39bb6ab8174c4
eobsite1 <30572208+eobsite1@users.noreply.github.com>
2018-03-27 22:22:20 +0100

    Update GPGWIZ.sh
    UNFINISHED DO NOT PULL UNTIL I FINISH

--- GPGWIZ.sh
+++ GPGWIZ.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+#GPGWIZ By eobsite1
+clear
 echo "Hello! welcome to eobsite1's GPG Wizard"
+read A
+clear
+echo "Lets make your key shall we?"
+read A
+clear
+gpg --gen-key
+clear
+echo "That was pretty painless right?"
+read A
+clear
+echo "Now we need a revoke certificate"
+read A
+clear
+gpg --output ~/CERTSNSHIT/revoke.crt --gen-revoke $GPGUSRID
